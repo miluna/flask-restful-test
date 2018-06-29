2986f02d0d77f758f25fa2b27bff6893e6751afb
Miguel Angel Luna <miluna1994@gmail.com>
2018-06-30 01:58:53 +0200

    Changed id value to proper one

--- src/resources/todos.py
+++ src/resources/todos.py
@@ -48,6 +48,5 @@ class TodoList(Resource):
     def post(self):
         args = parser.parse_args()
         todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
         TODOS[todo_id] = {'task': args['task']}
         return TODOS[todo_id], 201
